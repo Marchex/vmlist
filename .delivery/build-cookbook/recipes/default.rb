5f0e29b070b4d98b2f9b78cb9f96bf4c0c3c571f
Jimmy Carter <jcarter@marchex.com>
2016-06-20 16:03:40 -0700

    removing bits that were custom for mccx-auth (where I copied this from)

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -4,5 +4,3 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 include_recipe 'delivery-truck::default'
-
-chef_gem 'chef-rewind'
--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -4,12 +4,4 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-chef_gem "net_ldap" do
-  package_name 'net-ldap'
-  version '= 0.12.1'
-  compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
-  action :nothing
-end.run_action(:install)  # install at compile time to avoid Last3 library requiring 'net-ldap' before it's installed
-                          # see https://tickets.opscode.com/browse/CHEF-3696
-
 include_recipe 'delivery-truck::unit'
